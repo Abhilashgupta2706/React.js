521e6ffbf9a6c3bdadcb976e5a286a9f5ffa9b6e
Abhilash Gupta <abhilash@briot.in>
2025-05-13 12:05:53 +0530

    Dynamic List Content

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -32,11 +32,15 @@ function App() {
                     <h2>Core Concepts</h2>
 
                     <ul>
-                        <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
+                        {/* 2 ways of rendering */}
+                        {/* <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
                         <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image} />
-                        {/* Short and easy way */}
                         <CoreConcept {...CORE_CONCEPTS[2]} />
-                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+
+                        {/* Dynamic rendering without repetition of code */}
+                        {/* 'key' attribute is required so that react can identify the unique data after rendering */}
+                        {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
                     </ul>
 
                 </section>
