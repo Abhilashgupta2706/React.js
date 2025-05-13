232c107258de26c230d8bdf0d439a97291ae3b2c
Abhilash Gupta <abhilash@briot.in>
2025-05-13 10:46:27 +0530

    On Click event in component

--- 01-starting-project/src/components/TabButton/TabButton.jsx
+++ 01-starting-project/src/components/TabButton/TabButton.jsx
@@ -1,3 +1,12 @@
 export default function TabButton(props) { // Object destructuring is also a way
-    return <li><button>{props.children}</button></li>;
+    function handleClick() {
+        console.log("Hellow World")
+    }
+
+    return (
+        <li>
+            {/* Adding paranthesis on function name will make it execute on element render =>handleClick() */}
+            <button onClick={handleClick}>{props.children}</button>
+        </li>
+    );
 }
